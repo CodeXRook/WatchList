61650b0accfe31514624ac3f553aad34fbdd7791
CodeXRook <lacekitfly@gmail.com>
2019-09-20 13:37:05 -0400

    updated user 2 shows G.O.T

--- backend_/seed.sql
+++ backend_/seed.sql
@@ -42,4 +42,6 @@ INSERT INTO users (username) VALUES ('Pam Beesly'); -- 4
 
 -- INSERT SHOWS
 INSERT INTO shows (title, img_url, user_id, genre_id) 
-VALUES ('Game of Thrones', 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg',1 4);
+VALUES ('Game of Thrones', 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg', 1, 4);
+INSERT INTO shows (title, img_url, user_id, genre_id)
+VALUES ('Game of Thrones','https://image.tmdb.org/t/p/w370_and_h556_bestv2/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg', 2, 4);
