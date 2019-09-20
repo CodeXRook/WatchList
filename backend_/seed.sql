0a746e0ca4a4d39d256dce91fe29e684d658ef21
CodeXRook <lacekitfly@gmail.com>
2019-09-20 00:16:50 -0400

    show table completed

--- backend_/seed.sql
+++ backend_/seed.sql
@@ -7,3 +7,16 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL
 );
+
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    genre_name VARCHAR NOT NULL
+);
+
+CREATE TABLE shows (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    img_url VARCHAR NOT NULL,
+    user_id INT REFERENCES users(id),
+    genre_id INT REFERENCES genres (id)
+);
