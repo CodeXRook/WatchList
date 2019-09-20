77971f35d55f381a4f20f627b952e5843859030b
CodeXRook <lacekitfly@gmail.com>
2019-09-20 15:47:01 -0400

    updated user show for user 2

--- backend_/seed.sql
+++ backend_/seed.sql
@@ -56,3 +56,4 @@ VALUES ('The Flash','https://image.tmdb.org/t/p/w370_and_h556_bestv2/jC1KqsFx8Zy
 INSERT INTO shows (title, img_url, user_id, genre_id)
 VALUES ('Naruto Shippuden', 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/zAYRe2bJxpWTVrwwmBc00VFkAf4.jpg', 1, 4);
 INSERT INTO shows (title,img_url, user_id, genre_id)
+VALUES ('Naruto Shippun','https://image.tmdb.org/t/p/w370_and_h556_bestv2/zAYRe2bJxpWTVrwwmBc00VFkAf4.jpg', 2, 4);
